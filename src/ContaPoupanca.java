7487567eced25b9a22fbf400158be822d760fc62
Leonardo <leogrossms@gmail.com>
2024-06-13 14:05:15 -0300

    Finalizando projeto de Simulação de Conta Bancária

--- src/ContaPoupanca.java
+++
@@ -1,6 +0,0 @@
-public class ContaPoupanca extends Conta{
-    public void imprimirExtrato() {
-        System.out.println("Extrato Conta Poupcança ===");
-        super.imprimirInfosComuns();
-    }
-}
